7b230b43fd16f5d85a4873648c46fcb9567f108f
almadendj <almadendj@gmail.com>
2024-09-21 17:57:53 +0800

    add etherscan api

--- hardhat.config.js
+++ hardhat.config.js
@@ -17,6 +17,10 @@ module.exports = {
     sepolia: {
       url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
       accounts: [process.env.PRIVATE_KEY]
+    },
+    etherscan: {
+      apiKey: process.env.ETHERSCAN_API_KEY,
+      url: "https://api-sepolia.etherscan.io/api",
     }
   }
 };
